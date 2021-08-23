07b70d04a1109dc4c57dca97c8da0a35c8d3fa96
julienOlivier3 <julian-doerr@gmx.de>
2021-08-23 20:48:43 +0200

    Managed to extract table of carsharing offerings given location

--- 01_scrape_bcs.R
+++ 01_scrape_bcs.R
@@ -1,14 +1,8 @@
 library(rvest)
 library(RSelenium)
 
-bcs_landing <- 'https://carsharing.de/cs-standorte'
-
-bcs <- read_html(bcs_landing)
-
-bcs
-
-gemeinde1 <- "71134  AIDLINGEN"
-gemeinde2 <- "71229  LEONBERG"
+district <- "71134  AIDLINGEN"
+district <- "71229  LEONBERG"
 
 # Initialize Selenium server ----------------------------------------------
 remDr <- rsDriver(
@@ -18,18 +12,27 @@ remDr <- rsDriver(
   chromever = "88.0.4324.27"
 )
 
+# Define landing page
+bcs_landing <- 'https://carsharing.de/cs-standorte'
+
 # Navigate to landing page
 remDr$client$navigate(bcs_landing)
 
-# Find search field
+# Find search field on the landing page in order to insert the district where carsharing offers shall be searched for
 search_field <- remDr$client$findElement(using = "xpath", value = "//*[@id='edit-street-address']")
 
-# Paste district name into search field
-search_field$sendKeysToElement(list(gemeinde2, key="enter"))
+# Paste district name into search field and execute
+search_field$sendKeysToElement(list(district, key="enter"))
+
+# Refresh html code so hidden tables with carsharing locations in the given district gets updated
+remDr$client$refresh()
+
+# Parse updated html code
+parsed_html <- read_html(remDr$client$getPageSource()[[1]])
+
+# Get table with carsharing offerings
+parsed_html %>% 
+  html_nodes('table') %>% 
+  html_table()
 
 
-temp <- remDr$client$findElement(using = "xpath", value = "/html/body/div[2]/div/div/div[2]/div[3]")
-temp$click()
-car_list$
-car_list$click()
-car_list$sendKeysToElement(list(key="enter"))
